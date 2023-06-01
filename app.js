508a97ae7fd8fa96d6961ca909996a799dd90ee6
nikhil-9971 <124913952+nikhil-9971@users.noreply.github.com>
2023-06-01 23:12:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const roname = document.getElementById("roname");
+
+roname.addEventListener("input", () => {
+  roname.value = roname.value.toUpperCase();
+});
+
+const atgproberequirment = document.getElementById("atgproberequirment");
+
+atgproberequirment.addEventListener("input", () => {
+  atgproberequirment.value = atgproberequirment.value.toUpperCase();
+});
+
+document.querySelectorAll('input[type = "number"]').forEach(input => {
+  input.oninput = () => {
+    if (input.value.length > input.maxLength)
+      input.value = input.value.slice(0, input.maxLength);
+  };
+});
